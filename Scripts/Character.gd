12bbf01fb2d2183d4ed155d70ae50253acc4725d
Graf Eisen <grafeisenn@gmail.com>
2022-11-03 23:45:18 -0300

    good morning sirs, created character template for testing best regards

---
+++ Scripts/Character.gd
@@ -0,0 +1,66 @@
+extends KinematicBody2D
+
+var speed = 300
+var directions = ["idle_down","idle_up","idle_left","idle_right"]
+var dir = 0
+var state_machine
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	state_machine = $AnimationTree.get("parameters/playback")
+	state_machine.start("idle_down")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_pressed("move_down"):
+		move_down()
+	elif Input.is_action_pressed("move_up"):
+		move_up()
+	elif Input.is_action_pressed("move_left"):
+		move_left()
+	elif Input.is_action_pressed("move_right"):
+		move_right()
+	else:
+		state_machine.travel(directions[dir])
+	pass
+
+
+func move_down():
+	var velocity = move_and_slide(Vector2(0.0,1.0) * speed)
+	# Check that we're moving
+	if velocity.length_squared() > 0.5:
+		state_machine.travel("walk_down")
+	else:
+		state_machine.travel("idle_down")
+	dir = 0
+	pass
+
+func move_up():
+	var velocity = move_and_slide(Vector2(0.0,-1.0) * speed)
+	# Check that we're moving
+	if velocity.length_squared() > 0.5:
+		state_machine.travel("walk_up")
+	else:
+		state_machine.travel("idle_up")
+	dir = 1
+	pass
+
+func move_left():
+	var velocity = move_and_slide(Vector2(-1.0,0.0) * speed)
+	# Check that we're moving
+	if velocity.length_squared() > 0.5:
+		state_machine.travel("walk_left")
+	else:
+		state_machine.travel("idle_left")
+	dir = 2
+	pass
+
+func move_right():
+	var velocity = move_and_slide(Vector2(1.0,0.0) * speed)
+	if velocity.length_squared() > 0.5:
+		state_machine.travel("walk_right")
+	else:
+		state_machine.travel("idle_right")
+	dir = 3
+	pass
