53c511885ebbcfd436f41f5bd544df1a2e933541
david094 <daaf1994@gmail.com>
2022-11-20 22:06:26 -0300

    add interactive objects

--- Scripts/InteractableObject.gd
+++ Scripts/InteractableObject.gd
@@ -4,8 +4,8 @@ extends StaticBody2D
 export (int) var points = 3
 export (int) var resistance = 1
 export (String) var object_name = "interactive_object"
+export (bool) var multiple_hits = false
 var is_destroyed : bool = false
-
 
 signal object_destroyed
 
@@ -13,16 +13,33 @@ func _ready():
 	if $AnimatedSprite != null:
 		$AnimatedSprite.play("normal")
 
-func destroy_object():
+func damage_object(resistance):
 	$FCTManager.show_value(points)
-	emit_signal("object_destroyed", points)
-	$AnimationPlayer.play("die")
+#	emit_signal("object_destroyed", points)
+	$AnimatedSprite.play("broken"+String(resistance))
+
+func destroy_object():
+	if !multiple_hits:
+		$FCTManager.show_value(points)
+		emit_signal("object_destroyed", points)
+		$AnimatedSprite.play("broken")
+	else:
+		$AnimatedSprite.play("broken1")
+	
 
 func damage(item):
 	if !is_destroyed and item.can_interact_with_object(object_name):
-		resistance -= 1
-		$AnimationPlayer.play("dmg")
-		if resistance <= 0:
-			is_destroyed = true
-			destroy_object()
-		
+		if !multiple_hits:
+			resistance -= 1
+			$AnimationPlayer.play("dmg")
+			if resistance <= 0:
+				is_destroyed = true
+				destroy_object()
+		else:
+			resistance -= 1
+			print(resistance)
+			damage_object(resistance)
+			$AnimationPlayer.play("dmg")
+			if resistance <= 1:
+				is_destroyed = true
+				destroy_object()
--- Scripts/PlayerGlobalData.gd
+++ Scripts/PlayerGlobalData.gd
@@ -3,6 +3,9 @@ extends Node
 
 var axe = preload("res://Scenes/Items/Axe.tscn")
 var rock = preload("res://Scenes/Items/Rock.tscn")
+var flamethrower = preload("res://Scenes/Items/flamethrower.tscn")
+var eggs = preload("res://Scenes/Items/eggs.tscn")
+var spray = preload("res://Scenes/Items/spray.tscn")
 var player_neighbour_pos = Vector2(170, 258)
 
 # Array 
@@ -13,4 +16,6 @@ var inventory = []
 func _ready():
 	inventory.append(axe)
 	inventory.append(rock)
-	
+	inventory.append(flamethrower)
+	inventory.append(eggs)
+	inventory.append(spray)
