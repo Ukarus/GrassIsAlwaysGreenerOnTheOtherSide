a9bd80f70654304c2ff1b3e2518e45960f41fcc3
Ukarus <juan.llanosc@utem.cl>
2022-11-30 22:10:45 -0300

    add: endgame scene

--- Scripts/UI/EndGameScene.gd
+++ Scripts/UI/EndGameScene.gd
@@ -2,17 +2,30 @@ extends CanvasLayer
 
 
 export (bool) var player_won = false
-export (String) var winner_name 
+#export (String) var winner_name 
+onready var btn = $Control/Button
+
+const bad_ending = preload("res://Images/Ending/bad_ending.png")
+const good_ending = preload("res://Images/Ending/good_ending.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var pw = true if TimeTracker.winner.owner_name == "Player" else false
-	if pw:
-		$Control/Label.text = "You won"
+	btn.grab_focus()
+	var winner_name = ""
+	if TimeTracker.winner != null:
+		winner_name = TimeTracker.winner.owner_name
 	else:
-		$Control/Label.text = "You lost against {n}\n Better luck next time!".format({"n": TimeTracker.winner.owner_name})
+		winner_name = "Jonas"
+		
+	if winner_name == "Player":
+		$Control/TextureRect.texture = good_ending
+	else:
+		$Control/TextureRect.texture = bad_ending
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Button_button_up():
+	get_tree().quit()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
